5de65476d0a0f8019f5969d8d190ecbd2d1023d2
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-08-12 11:11:23 +0200

    refactored login

---
+++ dq-server/src/routes/login.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { loginSchema } = require('../validation/login');
+const userController = require('../controllers/users');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+router.use(express.json());
+
+router.post('/', async (req, res) => {
+	let result = loginSchema.validate(req.body);
+	if (result.error) {
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
+	result = await userController.getUserByName(req.body.username);
+	if (!result) return res.status(400).send('Invalid email or password');
+    
+	const isValidPassword = await bcrypt.compare(req.body.password, result.password);
+	if (!isValidPassword) return res.status(400).send('Invalid email or password'); 
+    
+	const token = result.generateAuthToken();
+	return res.send(token);
+});
+
+module.exports = router;
