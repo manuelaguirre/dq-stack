95d515aa604ae4ea3b3564a81f80a187c4eb19e6
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-08-12 10:03:04 +0200

    async wrapping users route

--- dq-server/src/routes/users.js
+++ dq-server/src/routes/users.js
@@ -2,25 +2,26 @@ const auth = require('../middleware/auth');
 const express = require('express');
 const userController = require('../controllers/users');
 const { createUserSchema } = require('../validation/input');
+const asyncCatch = require('../middleware/asyncCatch');
 const router = express.Router();
 router.use(express.json());
 
 
-router.get('/', auth, async (req, res) => {
+router.get('/', auth, asyncCatch(async (req, res) => {
 	const users =  await userController.getUsers();
 	if (req.query.username) {
 		const userByName = await userController.getUserByName(req.query.username.toString());
 		return res.send(userByName);
 	}   
 	return res.send(users);
-});
+}));
 
-router.get('/:id', auth, async (req, res) => {
+router.get('/:id', auth, (async (req, res) => {
 	const user = await userController.getUser(req.params.id);
 	return res.send(user);
-});
+}));
 
-router.post('/', async (req, res) => {
+router.post('/', asyncCatch(async (req, res) => {
 	let result = createUserSchema.validate(req.body);
 	if (result.error) {
 		res.status(400).send(result.error.details[0].message);
@@ -31,6 +32,6 @@ router.post('/', async (req, res) => {
 	result = await userController.createUser(req.body);	
 	const token = result.generateAuthToken();
 	return res.header('x-auth-token', token).send('User created.');
-});
+}));
 
 module.exports = router;
