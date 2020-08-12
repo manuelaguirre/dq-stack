c03e1765293e3730ee54956d317fba260744edf8
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-08-12 23:32:36 +0200

    fixed invalid token response code

--- dq-server/src/middleware/auth.js
+++ dq-server/src/middleware/auth.js
@@ -9,7 +9,7 @@ function auth(req, res, next) {
 		req.user = decoded;
 		next();
 	} catch (error) {
-		return res.status(400).send('Invalid Token');
+		return res.status(401).send('Invalid Token');
 	}
 }
 
