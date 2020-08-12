fef5a4c838f7b2854232e833f9494fe6b4d01c3e
Sebastian Fernandez <sfernandez1@uc.cl>
2020-08-12 20:25:38 +0200

    #2014 Correct typo

--- dq-backoffice/src/app/state.ts
+++ dq-backoffice/src/app/state.ts
@@ -1,8 +1,8 @@
 import { DqTheme } from './shared/models/dq-theme';
-import { DQQuestion } from './shared/models/dq-questions';
+import { DqQuestion } from './shared/models/dq-questions';
 
 export interface State {
   token: string;
   themes: DqTheme[];
-  questions: DQQuestion[];
+  questions: DqQuestion[];
 }
