fc529bc98cc063ae9db4dbf8d2e3174cbc6072c8
Sebastian Fernandez <sfernandez1@uc.cl>
2020-08-12 20:25:38 +0200

    #2014 Correct typo

--- dq-backoffice/src/app/back-office/resolvers/dq-backoffice-page.resolver.ts
+++ dq-backoffice/src/app/back-office/resolvers/dq-backoffice-page.resolver.ts
@@ -3,17 +3,17 @@ import {
     ActivatedRouteSnapshot, Resolve, RouterStateSnapshot,
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { DQQuestion } from '../../shared/models/dq-questions';
+import { DqQuestion } from '../../shared/models/dq-questions';
 import { DqTheme } from '../../shared/models/dq-theme';
 import { BackofficeService } from '../components/shared/services/backoffice.service';
 
 @Injectable()
-export class DqBackOfficeResolver implements Resolve<Partial<DQQuestion>[] | Partial<DqTheme>[]> {
+export class DqBackOfficeResolver implements Resolve<Partial<DqQuestion>[] | Partial<DqTheme>[]> {
   constructor(
     private backOfficeService: BackofficeService,
   ) {}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Partial<DQQuestion>[] | Partial<DqTheme>[]> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Partial<DqQuestion>[] | Partial<DqTheme>[]> {
     if (state.url.includes('themes')) {
       return this.backOfficeService.getThemes();
     }
