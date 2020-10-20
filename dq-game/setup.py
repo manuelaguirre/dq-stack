a000cb372fd4b35a9550414ca76f07becceeeada
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-10-20 19:46:46 +0200

    change setup.py

--- dq-game/setup.py
+++ dq-game/setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
 
-setup(name='dq-game', version='0.1.1', packages=find_packages())
+setup(
+    name='dq-game',
+    version='0.1.2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,)
