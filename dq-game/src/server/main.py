5a0332710d268bacc3ec195f94c559538e1fff44
Sebastian Fernandez <sfernandez1@uc.cl>
2020-10-24 09:52:32 +0200

    #6003 create renderer utils, clean main.py file

--- dq-game/src/server/main.py
+++ dq-game/src/server/main.py
@@ -9,15 +9,17 @@ from game import DQGame
 from renderer import ServerRenderer
 
 api_handler = APIHandler()
-
 controller = Controller()
 
-
 def start_game():
+  # Create main classes
   players = socket.clients.keys()
   dq_game = DQGame(players)
-  renderer = ServerRenderer(dq_game.start)
+  renderer = ServerRenderer()
+  # Bind events
+  renderer.on('start_game', dq_game.start)
   dq_game.on('show_instructions', renderer.show_instructions)
+  # Start
   renderer.initialize()
 
 socket.on('start-game', start_game)
@@ -25,4 +27,3 @@ socket.on('start-game', start_game)
 socket.listen(2)
 
 dq_game.on("please choose themes", controller.get_theme_choices())
-
