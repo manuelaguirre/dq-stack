cc697e4548770fcd61454cddb4d89d48126ef21e
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-10-27 23:15:00 +0100

    Added tests for controller

--- dq-game/test/unit/test_controller.py
+++ dq-game/test/unit/test_controller.py
@@ -1,39 +1,106 @@
 import os
 import sys
 import pytest
-from mock import patch, Mock
+from mock import patch, Mock, MagicMock
+
 from server.controller import Controller
 from utils.message import Message
-import utils.socket_connection
+from utils.socket_connection import ServerSocketConnection
 
-@patch('utils.socket_connection.ServerSocketConnection')
-def test_my_method_shouldCallMyClassMethodMyMethod_whenSomeOtherClassMethodIsCalled(self, mock_my_class):
-    some_other_class =  SomeOtherClassThatUsesMyClass()
-    some_other_class.method_under_test()
-    self.assertTrue(mock_my_class.called)
+    
+NO_OF_PLAYERS = 5
 
 class MockSocket:
+    
+    def __init__(self):
 
-    inb = [
-        Message("Pedro", "theme-choice", "ThemePedro"),
-        Message("Pablo", "themes-choice", "ThemePablo"),
-        Message("Palito", "themes-choice", "ThemePalito")
-    ]
+        self.listen = MagicMock()
+        self.on = MagicMock()
+        self.send_to_all = MagicMock()
 
-NO_OF_PLAYERS = 3    
-controller = Controller(3)
-mock_socket = MockSocket()
+        self.socket_key1 = MagicMock() #            <socket.socket fd=5, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.1.1', 8000), raddr=('127.0.0.1', 44772)>: {
+        self.socket_key2 = MagicMock() #            <socket.socket fd=6, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.1.1', 8000), raddr=('127.0.0.1', 44778)>: 
+        self.socket_key3 = MagicMock() # <socket.socket fd=9, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.1.1', 8000), raddr=('127.0.0.1', 57436)>
+        self.socket_key4 = MagicMock() # <socket.socket fd=8, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.1.1', 8000), raddr=('127.0.0.1', 57434)>
+        self.socket_key5 = MagicMock() # <socket.socket fd=7, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.1.1', 8000), raddr=('127.0.0.1', 57430)>
+
+
+
+        self.clients = {
+            self.socket_key1: {
+                'clientsocket': self.socket_key1, 
+                'address': ('127.0.0.1', 44772), 
+                'client_name': "Pedro"
+            },
+            self.socket_key2: 
+            {
+                'clientsocket': self.socket_key2, 
+                'address': ('127.0.0.1', 44778), 
+                'client_name': "Pablo"
+            },
+            self.socket_key3: 
+            {
+                'clientsocket': self.socket_key3, 
+                'address': ('127.0.0.1', 57436), 
+                'client_name': "Ricardo"
+            },
+            self.socket_key4: 
+            {
+                'clientsocket': self.socket_key4, 
+                'address': ('127.0.0.1', 57434), 
+                'client_name': "Marcos"
+            },
+            self.socket_key5: 
+            {
+                'clientsocket': self.socket_key5, 
+                'address': ('127.0.0.1', 57430), 
+                'client_name': "Eduardo"
+            }
+        }
+
+    
 
 class TestGetThemeChoices:
+    socket = MockSocket()
 
-    def test_get_theme_choices(self, monkeypatch):
-        
-        monkeypatch.setattr(ServerSocketConnection, "inb", mock_socket.inb)
-
-        #theme_list =  controller.get_theme_choices()
-
-        expected = ["ThemePedro", "ThemePablo", "ThemePalito"]
-
-        diff = set(theme_list) ^ set(expected)
+    def test_get_theme_choices_returns_set_from_inbuffer_msgs(self):
+        """
+        Should return a set eliminating repeated elements
+        """
        
-        assert not diff
+        socket = MockSocket()
+
+        socket.inbuffer = [
+            Message(socket.socket_key1, "THEME CHOICE", "Matemática"), 
+            Message(socket.socket_key3, "THEME CHOICE", "Historia"),
+            Message(socket.socket_key2, "THEME CHOICE", "Historia"),
+            Message(socket.socket_key4, "THEME CHOICE", "Sport"),
+            Message(socket.socket_key5, "THEME CHOICE", "Culture Génerale"),        
+        ]
+
+        controller = Controller(socket, NO_OF_PLAYERS)
+        result = controller.get_theme_choices()
+
+        assert set(result) == {"Historia", "Matemática", "Sport", "Culture Génerale"}
+
+class TestRequestThemeChoices:
+
+    def test_should_call_connection_send_method(self):
+        socket = MockSocket()
+        socket.send_to_all = MagicMock()
+
+        controller = Controller(socket, NO_OF_PLAYERS)
+        controller.request_theme_choices()
+        socket.send_to_all.assert_called_once_with("CHOOSE THEME", "event")
+
+    def test_should_trigger_wait_for_themes(self):
+        socket = MockSocket()
+        controller = Controller(socket, NO_OF_PLAYERS)
+        controller.trigger = MagicMock()
+
+        controller.request_theme_choices()
+
+        assert "wait-for-themes" in controller.callbacks
+
+        
+        
