7a416c560ed0a5b0c2febae8e54a91d759877a98
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-11-01 16:36:53 +0100

    Reformatted with black

--- dq-game/src/server/controller.py
+++ dq-game/src/server/controller.py
@@ -1,26 +1,48 @@
 import sys
 import os
+import pickle
 from utils.socket_connection import ServerSocketConnection
 from events.event_handler import EventHandler
 
 
 class Controller(EventHandler):
-    def __init__(self, no_of_players):
+    def __init__(self, socket, no_of_players):
         """
         Creates a controller
         """
+        self.socket = socket
+        self.socket.on("game_ready_to_start", self.start)
         self.no_of_players = no_of_players
-        self.socket = ServerSocketConnection(8000)
-        self.socket.on("game_ready_to_start", self.start_game)
+        self.players = []
 
-    def listen(self):
+    def start(self):
+        self.trigger("start_game")
+
+    def request_theme_choices(self, theme_list):
+
+        self.socket.send_to_all("CHOOSE THEME", "event")
+        self.socket.send_to_all(pickle.dumps(theme_list), "data-pickle")
+        self.trigger("THEMES REQUESTED")
+
+    def get_theme_choices(self):
+        result = []
+        for message in self.socket.inbuffer:
+            if message.content_type == "THEME":
+                result.append(message.data)
+
+        return result
+
+    def await_connections(self):
         self.socket.listen(self.no_of_players)
         self.players = self.socket.clients.keys()
 
-    def start_game(self):
-        print("controller start game")
-        self.trigger("controller_start_game")
 
-    def get_theme_choices(self):
-        # Send instruction to choose themes to clients
-        return False
+class ClientController(EventHandler):
+    def __init__(self, socket):
+        """
+        Creates a controller
+        """
+        self.socket = socket
+        self.socket.listen(no_of_players)
+        self.socket.on("game_ready_to_start", self.trigger("start_game"))
+        self.players = self.socket.clients.keys()
--- dq-game/src/server/main.py
+++ dq-game/src/server/main.py
@@ -8,8 +8,11 @@ from controller import Controller
 from game import DQGame
 from server_renderer import ServerRenderer
 
+NO_OF_PLAYERS = 1
+
+socket = ServerSocketConnection(8000)
 api_handler = APIHandler()
-controller = Controller(1)
+controller = Controller(socket, NO_OF_PLAYERS)
 
 
 def start_game():
@@ -17,12 +20,13 @@ def start_game():
     dq_game = DQGame()
     renderer = ServerRenderer()
     # Bind events
-    renderer.on("renderer_start_game", dq_game.start)
+    renderer.on("start_game", dq_game.start)
+    renderer.on("start_game", controller.request_theme_choices(["Matematica"]))
     dq_game.on("show_instructions", renderer.show_instructions)
-    dq_game.on("choose_themes", controller.get_theme_choices)
+    # dq_game.on("please choose themes", controller.get_theme_choices())
     # Start
     renderer.initialize()
 
 
-controller.on("controller_start_game", start_game)
-controller.listen()
+controller.on("start_game", start_game)
+controller.await_connections()
