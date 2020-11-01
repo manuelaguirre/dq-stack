b2109f90fdc03df3f7a578e6e9a90c22b4eeb055
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-11-01 16:53:51 +0100

    refactor  controller

--- dq-game/src/server/controller.py
+++ dq-game/src/server/controller.py
@@ -46,3 +46,35 @@ class ClientController(EventHandler):
         self.socket.listen(no_of_players)
         self.socket.on("game_ready_to_start", self.trigger("start_game"))
         self.players = self.socket.clients.keys()
+
+    def start(self):
+        self.trigger("start_game")
+
+    def request_theme_choices(self, theme_list):
+
+        self.socket.send_to_all("CHOOSE THEME", "event")
+        self.socket.send_to_all(pickle.dumps(theme_list), "data-pickle")
+        self.trigger("THEMES REQUESTED")
+
+    def get_theme_choices(self):
+        result = []
+        for message in self.socket.inbuffer:
+            if message.content_type == "THEME":
+                result.append(message.data)
+
+        return result
+
+    def await_connections(self):
+        self.socket.listen(self.no_of_players)
+        self.players = self.socket.clients.keys()
+
+
+class ClientController(EventHandler):
+    def __init__(self, socket):
+        """
+        Creates a controller
+        """
+        self.socket = socket
+        self.socket.listen(no_of_players)
+        self.socket.on("game_ready_to_start", self.trigger("start_game"))
+        self.players = self.socket.clients.keys()
