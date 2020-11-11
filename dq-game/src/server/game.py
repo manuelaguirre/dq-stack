73e0ac2af91e79f7a4858a34e6e3ed187f13939d
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-11-11 22:52:19 +0100

    removed triggers from server code

--- dq-game/src/server/game.py
+++ dq-game/src/server/game.py
@@ -9,9 +9,3 @@ class DQGame(EventHandler):
     def __init__(self, theme_list):
         self.theme_list = theme_list
         print("Creating a new game")
-
-    def start(self):
-        self.trigger("REQUEST_CHOOSE_THEMES", self.theme_list)
-
-    def show_instructions(self):
-        self.trigger("SHOW_INSTRUCTIONS")
