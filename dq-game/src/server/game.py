cac127e2e8144fd7a33965608016f42c0c877e6f
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-11-19 20:30:04 +0100

    6011: Annonce beggining of the first round, show message in central...

--- dq-game/src/server/game.py
+++ dq-game/src/server/game.py
@@ -1,5 +1,6 @@
 from events.event_handler import EventHandler
 from mock_data import mock_instructions
+from game_types.round import Round
 
 
 class DQGame(EventHandler):
@@ -11,12 +12,8 @@ class DQGame(EventHandler):
         self.no_of_players = no_of_players
         self.instructions = mock_instructions  # TODO: Get instructions from API ?
         self.available_themes = []
-        self.chosen_themes = []
-        self.game_questions = {
-            "first_round": [],
-            "second_round": [],
-            "third_round": [],
-        }
+        self.rounds = []
+            
         print("Creating a new game")
 
     def set_available_themes(self, themes):
@@ -26,12 +23,18 @@ class DQGame(EventHandler):
     def get_available_theme_names(self):
         return list(map(lambda theme: theme.name, self.available_themes))
 
-    def set_chosen_themes(self, themes):
-        self.chosen_themes = themes
+    def set_round_themes(self, themes):
+        for theme in themes:
+            self.rounds.append(Round(self.get_theme_by_name(theme)))  
         print("Chosen themes received")
 
-    def set_game_questions(self, questions):
-        self.game_questions["first_round"] = questions[0]
-        self.game_questions["second_round"] = questions[1]
-        self.game_questions["third_round"] = questions[2]
+    def get_theme_by_name(self, name):
+        for theme in self.available_themes:
+            if theme.name == name:
+                return theme
+        raise RuntimeError("No theme corresponds to this name")
+
+    def set_game_questions(self, questions_lists):
+        for i in range(len(self.rounds)):
+            self.rounds[i].set_questions(questions_lists[i])
         print("Questions received")
