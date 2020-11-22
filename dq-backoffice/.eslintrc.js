9b8b00b37eea098ca9ef9ed16eb7da9ab1878343
Sebastian Fernandez <sfernandez1@uc.cl>
2020-11-22 13:05:40 +0100

    ESLint rules

--- dq-backoffice/.eslintrc.js
+++ dq-backoffice/.eslintrc.js
@@ -1,17 +1,52 @@
 module.exports = {
-  "extends": ["eslint:recommended", "google"],
-  "rules": {
-    // enable additional rules
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "double"],
-    "semi": ["error", "always"],
-
-    // override default options for rules from base configurations
-    "comma-dangle": ["error", "always"],
-    "no-cond-assign": ["error", "always"],
-
-    // disable rules from base configurations
-    "no-console": "off",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "project": ["./tsconfig.json", "./tsconfig.app.json"],
+    "sourceType": "module"
   },
+  rules: {
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'no-prototype-builtins': 'off',
+    'no-plusplus': 'off',
+    'spaced-comment': 'off',
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": "error",
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": "error",
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 2,
+      { "SwitchCase": 1 }
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/no-object-literal-type-assertion": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        "singleline": {
+          "delimiter": "semi",
+          "requireLast": true
+        }
+      }
+    ],
+    "max-len": ["error", { "code": 120 }],
+    "class-methods-use-this": "off"
+  },
+  plugins: [
+    "@typescript-eslint",
+    "@typescript-eslint/tslint",
+  ],
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended"
+  ],
 };
