548112edd4a8d024044e346159af9e6164c56899
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-11-24 17:02:13 +0100

    hot fix

--- dq-server/src/app.js
+++ dq-server/src/app.js
@@ -13,7 +13,7 @@ const questions = require('./routes/questions');
 const themes = require('./routes/themes');
 const users = require('./routes/users');
 const fileImporter = require('./routes/fileImporter');
-const gameQuestions = require('./routes/gameQuestions');
+const questionPools = require('./routes/questionPools');
 
 app.use(cors(CORS_OPTIONS));
 app.use(morgan('dev'));
@@ -21,7 +21,7 @@ app.use(morgan('dev'));
 app.use('/api/login', login);
 app.use('/api/players', players);
 app.use('/api/questions', questions);
-app.use ('/api/gamequestions', gameQuestions);
+app.use ('/api/questionpools', questionPools);
 app.use('/api/themes', themes);
 app.use('/api/users', users);
 app.use('/api/import', fileImporter);
